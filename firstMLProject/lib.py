79bc4f85440d1bdf8bdb1e18341f5d6b083c43d9
RoselynYuer <yuehong929@outlook.com>
2022-08-08 14:22:07 +0800

    Initial Push

---
+++ firstMLProject/lib.py
@@ -0,0 +1,9 @@
+def try_me(name):
+    res=f'Hello {name}, welcome to the ML world!'
+    #print(res)
+    return res
+
+if __name__=="__main__":
+    name = 'Yue'
+    res = try_me(name)
+    print(res)
